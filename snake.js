bcb49146406bcf7b787754b540df2e10bedda72e
BekBrace <bekdevelopment@gmail.com>
2023-12-17 13:58:26 +0100

    Adding snake movement logic and binding the keys

--- snake.js
+++ snake.js
@@ -27,25 +27,72 @@ function getRandomInt(min, max) {
 let gameRunning = false;
 let gamePaused = false;
 
+let animationFrame;
+let startButton = document.querySelector('.start-button')
+let gameOverScreen = document.querySelector('.game-over')
+let scoreDisplay = document.querySelector('.score-display')
+
+startButton.addEventListener('click', function(){
+        // console.log("Button is clicked!")
+        startGame();
+    }
+);
+gameOverScreen.addEventListener('click', function(){
+        startGame();
+    }
+);
+
+document.addEventListener('keydown', function(e){
+    if (!gameRunning){
+        return;
+    }
+    // CODE 37 = LEFT KEY
+    if (e.which === 37 && snake.dx === 0){
+        snake.dx = -grid;
+        snake.dy = 0;
+        // CODE 38 = UP KEY
+    } else if (e.which === 38&& snake.dy === 0){
+        snake.dy = -grid;
+        snake.dx = 0;
+        // CODE 39 = RIGHT KEY
+    } else if (e.which === 39 && snake.dx === 0){
+        snake.dx = grid;
+        snake.dy = 0;
+      // CODE 40 = DOWN KEY
+    } else if (e.which === 40 && snake.dy === 0){
+        snake.dy = grid;
+        snake.dx = 0;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Game Loop Function
 function loop(){
     if (!gameRunning || gamePaused) {
         return;
     }
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
